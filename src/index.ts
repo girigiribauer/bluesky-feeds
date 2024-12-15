069f391a97d67f754880c55321171fc74bcb6db4
girigiribauer <girigiribauer@gmail.com>
2024-12-15 15:04:50 +0900

    雑に固定ポストを出すだけのフィード

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/.well-known/did.json", (c) =>
+  c.json({
+    "@context": ["https://www.w3.org/ns/did/v1"],
+    id: "did:web:helloworld.bsky.girigiribauer.com",
+    service: [
+      {
+        id: "#bsky_fg",
+        type: "BskyFeedGenerator",
+        serviceEndpoint: "https://helloworld.bsky.girigiribauer.com",
+      },
+    ],
+  })
+);
+
+app.get("/xrpc/app.bsky.feed.getFeedSkeleton", (c) =>
+  c.json({
+    feed: [
+      {
+        post: "at://did:plc:tsvcmd72oxp47wtixs4qllyi/app.bsky.feed.post/3ldcooerekc2y",
+      },
+      {
+        post: "at://did:plc:tsvcmd72oxp47wtixs4qllyi/app.bsky.feed.post/3ldcooerekc2y",
+      },
+    ],
+  })
+);
+
+serve(app);
