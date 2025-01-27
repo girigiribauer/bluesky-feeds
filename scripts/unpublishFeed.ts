02226d5db43645cce9ea7c12011b42e67872686c
girigiribauer <girigiribauer@gmail.com>
2025-01-27 22:52:47 +0900

    使わないけどunpublish修正

--- scripts/unpublishFeed.ts
+++ scripts/unpublishFeed.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { AtpAgent } from "@atproto/api";
+import { AVAILABLE_FEED_SERVICES, isFeedService } from "shared";
 
 (async () => {
   dotenv.config();
@@ -7,6 +8,20 @@ import { AtpAgent } from "@atproto/api";
   const password = process.env.APP_PASSWORD;
   if (!handle || !password) {
     console.log("invalid handle or password");
+    return;
+  }
+
+  if (process.argv.length <= 2 || !isFeedService(process.argv[2])) {
+    console.error("FeedService argument is missing.");
+    return;
+  }
+
+  const service = process.argv[2];
+  const feedService = AVAILABLE_FEED_SERVICES.find(
+    (a) => a.service === service
+  );
+  if (!feedService) {
+    console.error("FeedService definition is missing.");
     return;
   }
 
@@ -18,7 +33,7 @@ import { AtpAgent } from "@atproto/api";
   await agent.com.atproto.repo.deleteRecord({
     repo: agent.session?.did ?? "",
     collection: "app.bsky.feed.generator",
-    rkey: "helloworld",
+    rkey: feedService.service,
   });
 
   console.log("unpublish");
