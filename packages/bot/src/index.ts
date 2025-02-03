7db9ca5c434009b2d1b3039ed7c672135f4050ea
girigiribauer <girigiribauer@gmail.com>
2025-02-03 22:46:56 +0900

    定期ログを一旦消してみる

--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -37,19 +37,6 @@ export const createBot = async (): Promise<void> => {
     });
   });
 
-  scheduleJob("*/30 * * * *", async (fireDate: Date) => {
-    const time = new Intl.DateTimeFormat("ja-JP", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: false,
-      timeZone: "Asia/Tokyo",
-    }).format(fireDate);
-    console.log(`logging: ${time}`);
-  });
   // scheduleJob("42 */3 * * *", async (fireDate: Date) => {
   //   const time = new Intl.DateTimeFormat("ja-JP", {
   //     year: "numeric",
