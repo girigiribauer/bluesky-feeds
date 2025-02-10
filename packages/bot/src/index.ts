59608e7783aaf76cc33d514098e93236f746d5a6
girigiribauer <girigiribauer@gmail.com>
2025-02-10 10:12:57 +0900

    再びログ出力をオフにして問題が起きることを確認する

--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -37,19 +37,20 @@ export const createBot = async (): Promise<void> => {
     });
   });
 
-  scheduleJob("*/10 * * * *", async (fireDate: Date) => {
-    const time = new Intl.DateTimeFormat("ja-JP", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: false,
-      timeZone: "Asia/Tokyo",
-    }).format(fireDate);
-    console.log(`logging: ${time}`);
-  });
+  // scheduleJob("*/10 * * * *", async (fireDate: Date) => {
+  //   const time = new Intl.DateTimeFormat("ja-JP", {
+  //     year: "numeric",
+  //     month: "2-digit",
+  //     day: "2-digit",
+  //     hour: "2-digit",
+  //     minute: "2-digit",
+  //     second: "2-digit",
+  //     hour12: false,
+  //     timeZone: "Asia/Tokyo",
+  //   }).format(fireDate);
+  //   console.log(`logging: ${time}`);
+  // });
+
   // scheduleJob("42 */3 * * *", async (fireDate: Date) => {
   //   const time = new Intl.DateTimeFormat("ja-JP", {
   //     year: "numeric",
