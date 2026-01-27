8b72242a056748950899abd1404c98a7c23e3fca
girigiribauer <girigiribauer@gmail.com>
2026-01-27 14:23:26 +0900

    helloworldフィードのjetstream周りのデバッグ

--- rust-feeds/crates/jetstream/src/lib.rs
+++ rust-feeds/crates/jetstream/src/lib.rs
@@ -28,12 +28,20 @@ where
 
     let connector = JetstreamConnector::new(config)?;
     let receiver = connector.connect().await?;
+    tracing::info!("Jetstream connected successfully");
 
+    let mut count = 0;
     while let Ok(event) = receiver.recv_async().await {
+        count += 1;
+        if count % 1000 == 0 {
+            tracing::info!("Jetstream heartbeat: received {} events", count);
+        }
         if let JetstreamEvent::Commit(event) = event {
             callback(&event);
         }
     }
+    tracing::warn!("Jetstream receiver loop exited (connection closed?)");
+
 
     Ok(())
 }
