a8c875ebae7ee7696b4162b94324bd5f6101743a
girigiribauer <girigiribauer@gmail.com>
2026-01-27 14:47:04 +0900

    デバッグログ削除

--- rust-feeds/crates/jetstream/src/lib.rs
+++ rust-feeds/crates/jetstream/src/lib.rs
@@ -30,12 +30,7 @@ where
     let receiver = connector.connect().await?;
     tracing::info!("Jetstream connected successfully");
 
-    let mut count = 0;
     while let Ok(event) = receiver.recv_async().await {
-        count += 1;
-        if count % 1000 == 0 {
-            tracing::info!("Jetstream heartbeat: received {} events", count);
-        }
         if let JetstreamEvent::Commit(event) = event {
             callback(&event);
         }
